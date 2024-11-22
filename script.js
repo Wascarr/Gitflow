9b33b5723304a9f9e5964ec7c8c179b2c00f1bdd
Wascarr <babyprogrammer0@gmail.com>
2024-11-22 19:29:45 -0400

    Implementación avanzada del controlador de display

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
-let display = document.getElementById('display');
+// Sistema de Display Avanzado
+class DisplayController {
+    constructor() {
+        this.displayElement = document.getElementById('display');
+        this.history = [];
+        this.maxDigits = 12;
+    }
+
+    updateValue(value) {
+        if (this.isValidInput(value)) {
+            this.displayElement.value = this.formatValue(value);
+            this.history.push(value);
+        }
+    }
+
+    formatValue(value) {
+        if (typeof value === 'number') {
+            return value.toLocaleString('es-ES', {
+                maximumFractionDigits: 6,
+                minimumFractionDigits: 0
+            });
+        }
+        return value;
+    }
+
+    isValidInput(value) {
+        return value.toString().length <= this.maxDigits;
+    }
+
+    clear() {
+        this.displayElement.value = '0';
+        this.history = [];
+    }
+
+    getLastValue() {
+        return this.history[this.history.length - 1];
+    }
+
+    showError(message) {
+        this.displayElement.value = message || 'Error';
+        setTimeout(() => this.clear(), 2000);
+    }
+}
+
+const displayController = new DisplayController();
 
 function appendNumber(num) {
-    display.value += num;
+    displayController.updateValue(displayController.displayElement.value + num);
 }
 
 function appendOperator(operator) {
-    display.value += operator;
+    displayController.updateValue(displayController.displayElement.value + operator);
 }
 
 function clearDisplay() {
-    display.value = '';
+    displayController.clear();
 }
 
 function calculate() {
     try {
-        display.value = eval(display.value);
+        const result = eval(displayController.displayElement.value);
+        displayController.updateValue(result);
     } catch (error) {
-        display.value = 'Error';
+        displayController.showError('Error');
     }
 }
