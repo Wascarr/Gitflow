b981b5f2f4d0245c25e3d5b48dda7c64b8a7e34d
Wascarr <babyprogrammer0@gmail.com>
2024-11-22 19:32:17 -0400

    Implementación del sistema de entrada numérica

--- script.js
+++ script.js
@@ -19,3 +19,53 @@ function calculate() {
         display.value = 'Error';
     }
 }
+
+// Sistema de Entrada Numérica
+class NumberInputHandler {
+    constructor() {
+        this.currentInput = '';
+        this.maxLength = 12;
+        this.decimalAdded = false;
+    }
+
+    handleInput(value) {
+        if (this.isValidInput(value)) {
+            return this.processInput(value);
+        }
+        return false;
+    }
+
+    processInput(value) {
+        if (value === '.' && !this.decimalAdded) {
+            this.decimalAdded = true;
+            this.currentInput += value;
+            return true;
+        }
+
+        if (value !== '.') {
+            if (this.currentInput === '0' && value !== '0') {
+                this.currentInput = value;
+            } else if (this.currentInput !== '0') {
+                this.currentInput += value;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    isValidInput(value) {
+        const newLength = this.currentInput.length + 1;
+        return newLength <= this.maxLength;
+    }
+
+    reset() {
+        this.currentInput = '';
+        this.decimalAdded = false;
+    }
+
+    getCurrentValue() {
+        return this.currentInput || '0';
+    }
+}
+
+const numberHandler = new NumberInputHandler();
